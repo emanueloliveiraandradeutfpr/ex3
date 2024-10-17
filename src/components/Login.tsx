d2e2a25b3afb3a867b9ce3113b1838d6590a262f
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-10-17 16:23:31 -0300

    first commit

---
+++ src/components/Login.tsx
@@ -0,0 +1,53 @@
+import { View, TextInput, StyleSheet, Button } from "react-native";
+import React from "react";
+
+export default function Login() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        placeholder="Username"
+        value={username}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        placeholder="password"
+        value={password}
+        secureTextEntry
+      />
+
+      <Button
+        title="Logar"
+        color="#841584"
+        onPress={() => {
+          console.log(username);
+          console.log(password);
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
+    gap: 8,
+  },
+  input: {
+    backgroundColor: "#ddd",
+    height: 40,
+    width: 150,
+    padding: 10,
+    margin: 12,
+    borderColor: "#000",
+    borderWidth: 1,
+  },
+});
